585b77c6938856906ef180804790161ac45688b1
Pushkar Khare <kharepushkar2804@gmail.com>
2025-01-28 03:06:30 +0530

    added necessary utils,types and hooks

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+export const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [value, setValue] = useState<T>(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as const;
+};
